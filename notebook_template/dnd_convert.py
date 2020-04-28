9503821311888172638e2371b836d2e5d037fcc4
Vladislav Zaripov <mr.vladislavzaripov@mail.ru>
2020-04-28 22:47:43 +0500

    All merged

--- notebook_template/dnd_convert.py
+++ notebook_template/dnd_convert.py
@@ -1,6 +1,9 @@
 import sys
-import matplotlib.pylab as plt
 from Bio import Phylo
+
+'''Module dnd_convert is a utility module only
+to convert newick format to NEXUS format.
+It needs to plot phylogenetic trees with R module.'''
 
 in_filename = sys.argv[1] if len(sys.argv) >= 3 else snakemake.input
 out_filename = sys.argv[2] if len(sys.argv) >= 3 else snakemake.output
